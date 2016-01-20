9ae44ec37c34c018cb04fb11ae2db01a3c003c7b
Student <student@RU-102.local>
2016-01-20 16:39:19 -0700

    angular_day1_3

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ angular.module('angMod').controller('angoller', ['$scope',
 	function($scope) {
 
 		$scope.Part1 = function () {
-			console.log('Part1')
+			console.log('Part1');
+			$scope.buttonMessage = 'This is the Message in the button';
+		}
+
+		$scope.Part41 = function () {
+			console.log('Part1.4');
+			$scope.listMessage = 'This is the Message in the list';
 		}
 	
 	}])
