797985acbe1a78e94e65bfb984a75bdfbe2e6dcb
Student <student@RU-102.local>
2016-01-21 11:23:52 -0700

    angular_day2

--- script.js
+++ script.js
@@ -2,15 +2,27 @@
 angular.module('angMod', [])
 angular.module('angMod').controller('angoller', ['$scope', 
 	function($scope) {
-
 		$scope.Part1 = function () {
 			console.log('Part1');
 			$scope.buttonMessage = 'This is the Message in the button';
 		}
-
 		$scope.Part41 = function () {
 			console.log('Part1.4');
 			$scope.listMessage = 'This is the Message in the list';
 		}
-	
+		$scope.exclamation = function (which) {
+			console.log('exclamation')
+			if (which == 'h2') {
+				$scope.h2exclamationPoint = '!'
+			}
+			else {
+				$scope.exclamationPoint = '!'
+			}
+		}
+		$scope.confirmBox = function (event) {
+			var onclick = confirm("Do you wish to proceed?");
+				if (onclick == false) {
+					console.log(event.preventDefault())
+				}
+		}
 	}])
